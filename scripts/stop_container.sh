45d6819de49cc12fa63dde9cb56e7412694b02d1
Vishwadeep <70575947+Vishwadeep91@users.noreply.github.com>
2025-06-26 14:18:58 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,15 +2,7 @@
 set -e
 
 # Get container ID running the specified image
-containerID=$(docker ps --filter "ancestor=vishwadeep91/simple-python-flask-app:latest" -q)
-
-# Check if a container was found
-if [ -n "$containerID" ]; then
-  echo "Removing container: $containerID"
-  docker rm -f "$containerID"
-else
-  echo "No container found using image vishwadeep91/simple-python-flask-app:latest"
-fi
+echo "Hii"
 
 
 
